8c384f8043e4a8c76dcd0402d5f206a2dd757755
tktk_snsn <pw_tk.m0t06aro@outlook.com>
2023-07-25 07:56:22 +0900

    プロジェクト ファイルを追加します。

---
+++ CompetitiveProgrammingCs/Program.cs
@@ -0,0 +1,116 @@
+﻿using CompetitiveProgrammingCs.lib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solver solver = new Solver();
+            solver.solve(args);
+        }
+    }
+
+    class Solver
+    {
+        private IO.StreamScanner sc;
+        public Solver()
+        {
+            sc = new IO.StreamScanner(Console.OpenStandardInput());
+        }
+
+
+        public void solve(string[] args)
+        {
+            int px1 = sc.NextInt();
+            int py1 = sc.NextInt();
+            int px2 = sc.NextInt();
+            int py2 = sc.NextInt();
+
+            Point2D a = new Point2D(px1, py1);
+            Point2D b = new Point2D(px2, py2);
+
+            int q = sc.NextInt();
+            for (int i = 0; i < q; ++i)
+            {
+                int px3 = sc.NextInt();
+                int py3 = sc.NextInt();
+                Point2D c = new Point2D(px3, py3);
+
+                double proj = GeomUtils.Proj(c - a, b - a);
+                Point2D d = a + (b - a) * (proj / GeomUtils.Abs(b - a));
+                Point2D ans = c + 2 * (d - c);
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+namespace Program.IO
+{
+    using System.IO;
+    using System.Text;
+    using System.Globalization;
+
+    public class StreamScanner
+    {
+        private readonly Stream stream;
+        private readonly byte[] buffer = new byte[1024];
+        private int size, ptr;
+        private bool isEOF = false;
+
+        public StreamScanner(Stream stream)
+        {
+            this.stream = stream;
+            ptr = 0;
+            size = 0;
+        }
+
+        private byte read()
+        {
+            if (isEOF) return 0;
+            if (ptr >= size)
+            {
+                ptr = 0;
+                size = stream.Read(buffer, 0, 1024);
+                if (size <= 0)
+                {
+                    isEOF = true;
+                    return 0;
+                }
+            }
+            return buffer[ptr++];
+        }
+
+        public char Char()
+        {
+            byte b = 0;
+            do { b = read(); } while ((b < 33 || 126 < b) && !isEOF);
+            return (char)b;
+        }
+
+        public string Scan()
+        {
+            var sb = new StringBuilder();
+            for (var b = Char(); b >= 33 && b <= 126; b = (char)read())
+                sb.Append(b);
+            return sb.ToString();
+        }
+
+        public int NextInt() { return isEOF ? Int32.MinValue : Int32.Parse(Scan(), CultureInfo.InvariantCulture); }
+        public long NextLong() { return isEOF ? Int64.MinValue : Int64.Parse(Scan(), CultureInfo.InvariantCulture); }
+        public double NextDouble() { return isEOF ? Double.MinValue : Double.Parse(Scan(), CultureInfo.InvariantCulture); }
+    }
+}
---
+++ CompetitiveProgrammingCs/lib/geometry.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CompetitiveProgrammingCs.lib
+{
+    //using ftype = System.Int64;
+    using ftype = System.Double;
+
+    internal class Point2D
+    {
+        static readonly double Eps = 1E-9;
+        public ftype x { get; set; }
+        public ftype y { get; set; }
+
+        public Point2D() { }
+        public Point2D(ftype x, ftype y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point2D operator +(Point2D left, Point2D right) => new Point2D(left.x + right.x, left.y + right.y);
+        public static Point2D operator -(Point2D left, Point2D right) => new Point2D(left.x - right.x, left.y - right.y);
+        public static Point2D operator *(Point2D p, ftype v) => new Point2D(p.x * v, p.y * v);
+        public static Point2D operator *(ftype v, Point2D p) => p * v;
+        public static Point2D operator /(Point2D p, ftype v) => new Point2D(p.x / v, p.y / v);
+
+        public override string ToString()
+        {
+            return $"{x} {y}";
+        }
+    }
+
+    internal class Point3D
+    {
+        public ftype x { get; set; }
+        public ftype y { get; set; }
+        public ftype z { get; set; }
+
+        public Point3D() { }
+        public Point3D(ftype x, ftype y, ftype z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Point3D operator +(Point3D left, Point3D right) => new Point3D(left.x + right.x, left.y + right.y, left.z + right.z);
+        public static Point3D operator -(Point3D left, Point3D right) => new Point3D(left.x - right.x, left.y - right.y, left.z - right.z);
+        public static Point3D operator *(Point3D p, ftype v) => new Point3D(p.x * v, p.y * v, p.z * v);
+        public static Point3D operator *(ftype v, Point3D p) => p * v;
+        public static Point3D operator /(Point3D p, ftype v) => new Point3D(p.x / v, p.y / v, p.z / v);
+        public override string ToString()
+        {
+            return $"{x} {y} {z}";
+        }
+    }
+
+    internal static class GeomUtils
+    {
+        /// <summary>
+        /// Inner product(Dot Product)
+        /// </summary>
+        public static ftype Dot(Point2D a, Point2D b) => a.x * b.x + a.y * b.y;
+        /// <summary>
+        /// Inner product(Dot Product)
+        /// </summary>
+        public static ftype Dot(Point3D a, Point3D b) => a.x * b.x + a.y * b.y + a.z * b.z;
+
+        public static ftype Norm(Point2D a) => Dot(a, a);
+        /// <summary>
+        /// Length of a
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static double Abs(Point2D a) => Math.Sqrt(Norm(a));
+
+        /// <summary>
+        /// Projection of a onto b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Proj(Point2D a, Point2D b) => Dot(a, b) / Abs(b);
+
+        /// <summary>
+        /// Angle between two vectors a and b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Angle(Point2D a, Point2D b) => Math.Acos(Dot(a, b) / Abs(a) / Abs(b));
+
+        /// <summary>
+        /// cross product
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static ftype Cross(Point2D a, Point2D b) => a.x * b.y - b.x * a.y;
+        public static Point3D Cross(Point3D a, Point3D b) => new Point3D(
+            a.y * b.z - b.y * a.z,
+            a.z * b.x - b.z * a.x,
+            a.x * b.y - b.x * a.y
+            );
+        public static ftype Triple(Point3D a, Point3D b, Point3D c) => Dot(a, Cross(b, c));
+    }
+}
